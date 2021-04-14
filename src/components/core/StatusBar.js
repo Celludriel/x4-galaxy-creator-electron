c70bdfc9b9fff0ccc3d421a7aa0380e1cb05ff3b
kenny de smedt <de_smedt_kenny@hotmail.com>
2021-04-14 23:17:03 +0200

    name fix

--- src/components/core/StatusBar.js
+++ src/components/core/StatusBar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Segment, Table } from 'semantic-ui-react';
 
-function MenuBar({ galaxy, filePath }) {
+function StatusBar({ galaxy, filePath }) {
 
     return (
         <Segment>
             <Table>
                 <Table.Row key={0}>
-                    <Table.Cell >Macro: {galaxy.galaxyPrefix}_galaxy_macro</Table.Cell>
-                    <Table.Cell >File Path: {filePath}</Table.Cell>
+                    {
+                        galaxy.galaxyPrefix !== "" &&
+                            <Table.Cell >Macro: {galaxy.galaxyPrefix}_galaxy_macro</Table.Cell>
+                    }
+                    {
+                        filePath !== "" &&
+                            <Table.Cell >File Path: {filePath}</Table.Cell>
+                    }
                 </Table.Row>
             </Table>
         </Segment>
     )
 }
 
-export default MenuBar;
+export default StatusBar;
